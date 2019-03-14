ad5a81161203e773d4e3b614fecf3243f4fabe82
wufei0429 <2453804076@qq.com>
2019-03-14 13:46:51 +0800

    commit web module

---
+++ web/src/main/src/com/spring/web/dao/CourseDAO.java
@@ -0,0 +1,28 @@
+package com.spring.web.dao;
+
+import com.spring.web.entity.Course;
+import com.spring.web.entity.CourseVO;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface CourseDAO extends BaseDAO<Course> {
+
+    //自定义的多表关联查询
+    @Results({@Result(column = "course_id", property = "courseId"),
+            @Result(column = "course_name", property = "courseName"),
+            @Result(column = "user_id", property = "userId"),
+            @Result(column = "course_class", property = "courseClass"),
+            @Result(column = "cover", property = "cover"),
+            @Result(column = "course_code", property = "courseCode"),
+            @Result(column = "finished", property = "finished"),
+            @Result(column = "username", property = "username"),
+            @Result(column = "avatar", property = "avatar")
+    })
+    @Select("SELECT a.*,b.username,b.avatar\n" +
+            "FROM t_course a,t_sys_user b\n" +
+            "WHERE a.user_id = b.user_id\n ")
+    List<CourseVO> selectCurrentCourses();
+}
---
+++ web/src/main/src/com/spring/web/service/CourseService.java
@@ -0,0 +1,10 @@
+package com.spring.web.service;
+
+import com.spring.web.entity.CourseVO;
+
+import java.util.List;
+
+public interface CourseService {
+    List<CourseVO> selectCurrentCourses();
+
+}
